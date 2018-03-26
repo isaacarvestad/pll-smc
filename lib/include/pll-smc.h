8d77fa290ab60f575be759af23aebd822ec62405
Isaac Arvestad <isaac.arvestad@gmail.com>
2018-03-26 15:22:05 +0200

    Add outline of SMC api

--- lib/include/pll-smc.h
+++ lib/include/pll-smc.h
@@ -2,9 +2,29 @@
 #define LIB_PLL_SMC_H
 
 #include <string>
-
+#include <vector>
 #include <libpll/pll.h>
 
-std::string library_test();
+#include "particle.h"
+
+/**
+   Creates a vector of 'count' number of particles, each using the given vector
+   of sequences.
+
+   Each particle starts with a weight of 1/'count'.
+ */
+std::vector<Particle> create_particles(const unsigned int count,
+                                       std::vector<std::string> sequences);
+
+/**
+   Runs the Sequential Monte Carlo algorithm on a vector of input
+   particles. Returns the resulting particles.
+ */
+std::vector<Particle> run_smc(std::vector<Particle> particles,
+                              const unsigned int iterations);
+
+void resample(Particle &particle);
+void propose(Particle &particle);
+void weight(Particle &particle);
 
 #endif
--- lib/src/pll-smc.cpp
+++ lib/src/pll-smc.cpp
@@ -1,7 +1,34 @@
 #include "pll-smc.h"
 
-std::string library_test() {
-  pll_partition_t* partition = pll_partition_create(4,2,4,6,1,5,4,2,PLL_ATTRIB_ARCH_AVX);
+std::vector<Particle> create_particles(const unsigned int count,
+                                       const std::vector<std::string> sequences)
+{
+  const double initial_weight = 1.0f / (double) count;
 
-  return std::to_string(partition->tips);
+  std::vector<Particle> particles(count, Particle(initial_weight, sequences));
+
+  return particles;
+}
+
+std::vector<Particle> run_smc(std::vector<Particle> &particles,
+                              const unsigned int iterations)
+{
+  for (int i = 0; i < iterations; i++) {
+    for (auto &p : particles) {
+      resample(p);
+      propose(p);
+      weight(p);
+    }
+  }
+
+  return particles;
+}
+
+void resample(Particle &particle) {
+}
+
+void propose(Particle &particle) {
+}
+
+void weight(Particle &particle) {
 }
