9d32c74a95325190fa56c58bcf574e0a446c81f0
Isaac Arvestad <isaac.arvestad@gmail.com>
2018-03-27 22:50:25 +0200

    Add tree printing function, update sequences and output

--- app/main.cpp
+++ app/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <float.h>
 
 #include "pll_smc.h"
 
 
 void print_tree(pll_rnode_s* root) {
   if (root->left && root->right) {
-    std::cout << "(* : ";
+    std::cout << "(";
     print_tree(root->left);
     std::cout << ", ";
     print_tree(root->right);
-    std::cout << ")";
+    std::cout << "):" << root->length;
   } else {
     std::string label(root->label);
-    std::cout << label;
+    std::cout << label << ":" << root->length;
   }
 }
 
@@ -21,18 +22,23 @@ int main() {
   std::cout << "Hello world!" << std::endl;
 
   std::vector<std::pair<std::string, std::string>> sequences =
-    { {"H1", "TAAAAC"},
-      {"H2", "CACACG"},
-      {"H3", "AGGACA"},
-      {"H4", "CGTAGT"},
-      {"H5", "CGAATT"}
+    { {"H0", "TATTGCTAATGCTACATCGCTCGGAACAAATACGGGGCACTTGTTTTCAGCACAAGCAAGCGGGAAGTCATGACCCCCTAGAGTCCCTTCTGGAAAGCCT"},
+      {"H1", "GAGGCTACCCGCTGACAGTTCCCGAACAGGAATAATTATTGGGTGTTCTGATCGAACAGGGTATGGCAATGGCCATCACGAGCCGAGCGGCCTCCCAAAG"},
+      {"H2", "GATACATCGGCGTTTACTACCCCTAGCTGAGTTCCTTTCAGTTGCGTTTGGGCGATTCAGGTTTACCCGGAGACGAACTCACTCAACTTTTAAACTAAGA"},
+      {"H4", "TGAGACTGTTTGACGGTCTAGCGGATTCTCGTCAATGTAGTACCATATAGCGGGGCGTTCTCAACCATTTAGCGGAAATTAATGCGTTGACCCAGTCCCG"},
+      {"H5", "CGACTGCGGGACCGCGGCTGCAGGATAGTTAATTAATTAGCGGCTGTTATGTCGCGCCACTCATTAGGGGTACGGCTTTAATTGAAGGTAGGTATTCACG"},
+      {"H7", "CGTATCGAGTAACGCCCCCGATGGCTGGTGAGTAAATTAGGCTTGAATAGCGAGACGCACTACTCCGACTACAGATAAAAATTCAAGTTCCTGAGTCTCG"},
+      {"H8", "TCTTACGGAAGTCTTATGATATACGCTGTCAGCCCGCCACAGTGTTTAAATAATCATGCTGCGGAAGTGTTCGTATCATGGGAGTTCTGCAGACGCCATA"},
+      {"H12", "ACGCAACAGTCGTCTCTAGTTTTACGCAGGAGACTAGTGGCATGGGTTGGCTGTCACATCAGACTCCGGTGTCGTTCTGGGGAAATTGATCGAGGTTAGG"},
+      {"H15", "ATTGTCCACACACTCGTTAACAGGCAAAAAGTCCGTCTAAAAGGGCTTAGGTGGAGCTCGATCCTGCAGTTCAACATCGAGTAACCACACTCGTACTGCG"},
+      {"H16", "ATCCCTCTTACAAGCCTTTCCAGTCCATGGGGCTGAGTAAAATAGACAGTGCCTCGCGCGAGCACGGAGAATCTAATCATGAACCCCTCCGTCCTTCCGC"},
     };
 
   std::vector<Particle> particles = create_particles(10000, sequences);
-  particles = run_smc(particles, 4);
+  particles = run_smc(particles, sequences.size());
 
   Particle* particle = nullptr;
-  double max = 0.0f;
+  double max = -DBL_MAX;
   for (auto &p : particles) {
     if (p.weight > max) {
       max = p.weight;
@@ -41,9 +47,9 @@ int main() {
   }
   std::cout << std::endl;
 
-  std::cout << "Likelihood: " << particle->weight << std::endl;
+  std::cout << "Weight: " << particle->weight << ", normalized weight: " << particle->normalized_weight << std::endl;
 
   assert(particle->get_roots().size() == 1);
   print_tree(particle->get_roots()[0]);
-  std::cout << std::endl;
+  std::cout << ";" << std::endl;
 }
